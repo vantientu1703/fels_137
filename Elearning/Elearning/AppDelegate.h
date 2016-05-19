5dd3ba40c9c7b5af9501768c5d5e69402a665853
Nguyen Van Thieu B <nguyen.van.thieub@framgia.com>
2016-05-19 13:43:05 +0700

    Create new project Elearning Mobile

---
+++ Elearning/Elearning/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Elearning
+//
+//  Created by  on 5/19/16.
+//  Copyright © 2016 Framgia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
