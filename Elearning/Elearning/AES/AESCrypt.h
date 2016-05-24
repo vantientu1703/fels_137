7efdd916e12c241879ddc88fe6529ee7328162b8
Nguyen Van Thieu B <nguyen.van.thieub@framgia.com>
2016-05-24 16:37:40 +0700

    Encrypt and decrypt with AES

---
+++ Elearning/Elearning/AES/AESCrypt.h
@@ -0,0 +1,13 @@
+//
+//  AESCrypt.h
+//  Elearning
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AESCrypt : NSObject
+
++ (NSString *)encrypt:(NSString *)message password:(NSString *)password;
++ (NSString *)decrypt:(NSString *)base64EncodedString password:(NSString *)password;
+
+@end
